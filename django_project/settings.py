187616cd13db3b455ee5f3cb2de07a5933dae0c4
devwithshams <talktoshamsuddeen@gmail.com>
2025-03-06 23:17:52 +0000

    remove old stuff

--- django_project/settings.py
+++ django_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h2+23=)5fmor*6%rm&21$-uvj3x51hy1nig(h)royofagevzy$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['devwithshams.pythonanywhere.com']
 
 
 # Application definition
